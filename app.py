c661faba3791cb227c10e4e3c2f86770ff8b37cc
Kevin Potts <skydivekevin@gmail.com>
2018-10-29 15:51:14 -0600

    edited procfile
    Signed-off-by: Kevin Potts <skydivekevin@gmail.com>

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 import os
 import uuid
 
-from flask import Flask, jsonify, request
+from flask import Flask
 from flask_cors import CORS
 
-# configuration
 DEBUG = True
 
-# instantiate the app
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-# enable CORS
 CORS(app)
 
 
