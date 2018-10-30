c0147b80fe4c7ae4cfc8e09bdba5ac08e877c550
Kevin Potts <skydivekevin@gmail.com>
2018-10-29 18:58:51 -0600

    edited procfile to attempt to deploy to heroku
    Signed-off-by: Kevin Potts <skydivekevin@gmail.com>

--- app.py
+++ app.py
@@ -2,12 +2,18 @@ import os
 import uuid
 
 from flask import Flask
+from flask_pymongo import PyMongo
 from flask_cors import CORS
 
 DEBUG = True
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+app.config["MONGO_DBNAME"] = "skydivereviews"
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/skydivereviews"
+app.config["MONGO_URI"] = "mongodb://admin:password1@ds155192.mlab.com:55192/skydivereviews"
+mongo = PyMongo(app)
 
 CORS(app)
 
@@ -25,3 +31,10 @@ def addInstructor():
 @app.route("/login")
 def login():
     return "login page"
+
+
+# @app.route("/add")
+# def add():
+#     instructor = mongo.db.instructors
+#     instructor.insert({'firstName': 'Ballsack'})
+#     return 'added instructor!'
